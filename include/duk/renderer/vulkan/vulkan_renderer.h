2a07a7b87dadfdb5cff08457ef56d8131c16070d
Ricardo Novak <ricardonovak@yahoo.com.br>
2023-05-14 22:36:21 +0100

    new: add vulkan renderer

---
+++ include/duk/renderer/vulkan/vulkan_renderer.h
@@ -0,0 +1,31 @@
+//
+// Created by Ricardo on 04/02/2023.
+//
+
+#ifndef DUK_VULKAN_RENDERER_H
+#define DUK_VULKAN_RENDERER_H
+
+#include <duk/renderer/renderer.h>
+
+namespace duk {
+
+struct VulkanRendererCreateInfo {
+
+};
+
+class VulkanRenderer : public Renderer {
+public:
+
+    VulkanRenderer(const RendererCreateInfo& rendererCreateInfo, const VulkanRendererCreateInfo& vulkanRendererCreateInfo);
+
+    void begin_frame() override;
+
+    void end_frame() override;
+
+private:
+
+};
+
+}
+
+#endif //DUK_VULKAN_RENDERER_H
---
+++ src/duk/renderer/vulkan/vulkan_renderer.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Ricardo on 04/02/2023.
+//
+
+#include <duk/renderer/vulkan/vulkan_renderer.h>
+
+namespace duk {
+
+VulkanRenderer::VulkanRenderer(const RendererCreateInfo& rendererCreateInfo,
+                               const VulkanRendererCreateInfo& vulkanRendererCreateInfo) {
+
+}
+
+void VulkanRenderer::begin_frame() {
+
+}
+
+void VulkanRenderer::end_frame() {
+
+}
+
+
+}
