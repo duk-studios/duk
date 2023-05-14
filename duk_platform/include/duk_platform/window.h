2e636ed2ad2ca317b2aa4b3b48c75353b54f2d4a
Ricardo Novak <ricardonovak@yahoo.com.br>
2023-05-14 22:36:24 +0100

    new: add duk_platform

---
+++ duk_platform/include/duk_platform/window.h
@@ -0,0 +1,19 @@
+//
+// Created by Ricardo on 13/04/2023.
+//
+
+#ifndef DUK_PLATFORM_WINDOW_H
+#define DUK_PLATFORM_WINDOW_H
+
+namespace duk::platform {
+
+class Window {
+public:
+
+private:
+
+};
+
+}
+
+#endif //DUK_PLATFORM_WINDOW_H
---
+++ duk_platform/src/duk_platform/window.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Ricardo on 13/04/2023.
+//
+
+#include <duk_platform/window.h>
+
+namespace duk::platform {
+
+
+
+}
