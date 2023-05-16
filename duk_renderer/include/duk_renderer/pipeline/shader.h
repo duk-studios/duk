6c35879fed5bb7463d1f0b6a07287d5e6073df8f
Ricardo Novak <ricardonovak@yahoo.com.br>
2023-05-16 23:54:12 +0100

    fix: shader module bits enum order

--- duk_renderer/include/duk_renderer/pipeline/shader.h
+++ duk_renderer/include/duk_renderer/pipeline/shader.h
@@ -13,9 +13,9 @@ public:
     struct ModuleType {
         enum Bits : uint32_t {
             VERTEX = 1 << 0,
-            GEOMETRY = 1 << 1,
-            TESSELATION_CTRL = 1 << 2,
-            TESSELATION_EVAL = 1 << 3,
+            TESSELATION_CTRL = 1 << 1,
+            TESSELATION_EVAL = 1 << 2,
+            GEOMETRY = 1 << 3,
             FRAGMENT = 1 << 4,
             COMPUTE = 1 << 5,
         };
