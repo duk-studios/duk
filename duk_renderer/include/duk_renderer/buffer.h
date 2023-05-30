73dfad0d3ff0dc70cfd2ca33e50516ad5780490a
Ricardo Novak <ricardonovak@yahoo.com.br>
2023-05-30 19:47:34 +0100

    chg: use concepts to improve read/write methods on buffer

--- duk_renderer/include/duk_renderer/buffer.h
+++ duk_renderer/include/duk_renderer/buffer.h
@@ -8,6 +8,8 @@
 
 #include <cstdint>
 #include <cassert>
+#include <ranges>
+#include <type_traits>
 
 namespace duk::renderer {
 
@@ -32,18 +34,20 @@ public:
 
     virtual void read(void* dst, size_t size, size_t offset) = 0;
 
-    template<typename T>
-    void read(T& dst, size_t index) {
-        assert(sizeof(T) == element_size());
-        read(&dst, sizeof(T), index * sizeof(T));
+    template<std::ranges::contiguous_range T>
+    void read(T& dst, size_t startElement = 0) {
+        constexpr size_t elementSize = sizeof(std::ranges::range_value_t<T>);
+        assert(elementSize == element_size());
+        read((void*)std::data(dst), std::size(dst) * elementSize, startElement * elementSize);
     }
 
     virtual void write(void* src, size_t size, size_t offset) = 0;
 
-    template<typename T>
-    void write(const T& src, size_t index) {
-        assert(sizeof(T) == element_size());
-        write(&src, sizeof(T), index * sizeof(T));
+    template<std::ranges::contiguous_range T>
+    void write(const T& src, size_t startElement = 0) {
+        constexpr size_t elementSize = sizeof(std::ranges::range_value_t<T>);
+        assert(elementSize == element_size());
+        write((void*)std::data(src), std::size(src) * elementSize, startElement * elementSize);
     }
 
     DUK_NO_DISCARD virtual size_t size() const = 0;
