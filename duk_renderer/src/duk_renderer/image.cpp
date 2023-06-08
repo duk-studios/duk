38f8d8f4928bb881d4cefa33c5b1c9a96991af44
Ricardo Novak <ricardonovak@yahoo.com.br>
2023-06-08 15:20:40 +0100

    new: add depth formats

--- duk_renderer/src/duk_renderer/image.cpp
+++ duk_renderer/src/duk_renderer/image.cpp
@@ -29,6 +29,7 @@ size_t Image::pixel_format_size(Image::PixelFormat format) {
         case PixelFormat::B8G8R8_UNORM:
         case PixelFormat::B8G8R8_SNORM:
         case PixelFormat::B8G8R8_SRGB:
+        case PixelFormat::D16_UNORM_S8_UINT:
             size = 3;
             break;
         case PixelFormat::R8G8B8A8_UNORM:
@@ -37,12 +38,40 @@ size_t Image::pixel_format_size(Image::PixelFormat format) {
         case PixelFormat::B8G8R8A8_UNORM:
         case PixelFormat::B8G8R8A8_SNORM:
         case PixelFormat::B8G8R8A8_SRGB:
+        case PixelFormat::D32_SFLOAT:
+        case PixelFormat::D24_UNORM_S8_UINT:
             size = 4;
+            break;
+        case PixelFormat::D32_SFLOAT_S8_UINT:
+            size = 5;
             break;
         default:
             throw std::runtime_error("unhandled Image::Format on Image::pixel_format_size");
     }
     return size;
+}
+
+bool Image::is_depth_format(Image::PixelFormat format) {
+    switch (format) {
+        case PixelFormat::D32_SFLOAT:
+        case PixelFormat::D16_UNORM_S8_UINT:
+        case PixelFormat::D24_UNORM_S8_UINT:
+        case PixelFormat::D32_SFLOAT_S8_UINT:
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool Image::is_stencil_format(Image::PixelFormat format) {
+    switch (format) {
+        case PixelFormat::D16_UNORM_S8_UINT:
+        case PixelFormat::D24_UNORM_S8_UINT:
+        case PixelFormat::D32_SFLOAT_S8_UINT:
+            return true;
+        default:
+            return false;
+    }
 }
 
 }
