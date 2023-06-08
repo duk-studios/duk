50d73cc7ec67a811ab2d206d62404c8c3090109e
Ricardo Novak <ricardonovak@yahoo.com.br>
2023-06-08 15:22:18 +0100

    new: add EmptyImageDataSource

--- duk_renderer/include/duk_renderer/image_data_source.h
+++ duk_renderer/include/duk_renderer/image_data_source.h
@@ -6,7 +6,9 @@
 
 #include <duk_renderer/data_source.h>
 #include <duk_renderer/image.h>
+
 #include <cstring>
+#include <stdexcept>
 
 namespace duk::renderer {
 
@@ -22,6 +24,8 @@ public:
     DUK_NO_DISCARD virtual size_t width() const = 0;
 
     DUK_NO_DISCARD virtual size_t height() const = 0;
+
+    DUK_NO_DISCARD virtual bool has_data() const = 0;
 
     virtual void read_bytes(void* dst, size_t size, size_t offset) const = 0;
 
@@ -76,6 +80,10 @@ public:
         return m_height;
     }
 
+    DUK_NO_DISCARD bool has_data() const override {
+        return !m_pixels.empty();
+    }
+
     void read_bytes(void* dst, size_t size, size_t offset) const override {
         assert(byte_count() >= size + offset);
         const uint8_t* src = (const uint8_t*)m_pixels.data();
@@ -87,6 +95,9 @@ protected:
     DUK_NO_DISCARD duk::hash::Hash calculate_hash() const override {
         duk::hash::Hash hash = 0;
         duk::hash::hash_combine(hash, m_pixels.data(), byte_count());
+        duk::hash::hash_combine(hash, m_width);
+        duk::hash::hash_combine(hash, m_height);
+        duk::hash::hash_combine(hash, PixelFormat);
         return hash;
     }
 
@@ -94,6 +105,59 @@ private:
     uint32_t m_width;
     uint32_t m_height;
     std::vector<PixelType> m_pixels;
+};
+
+class EmptyImageDataSource : public ImageDataSource {
+public:
+    EmptyImageDataSource(uint32_t width, uint32_t height, Image::PixelFormat pixelFormat) :
+        m_width(width),
+        m_height(height),
+        m_pixelFormat(pixelFormat) {
+
+    }
+
+    DUK_NO_DISCARD Image::PixelFormat pixel_format() const override {
+        return m_pixelFormat;
+    }
+
+    DUK_NO_DISCARD size_t pixel_count() const override {
+        return m_width * m_height;
+    }
+
+    DUK_NO_DISCARD size_t byte_count() const override {
+        return pixel_count() * Image::pixel_format_size(m_pixelFormat);
+    }
+
+    DUK_NO_DISCARD size_t width() const override {
+        return m_width;
+    }
+
+    DUK_NO_DISCARD size_t height() const override {
+        return m_height;
+    }
+
+    DUK_NO_DISCARD bool has_data() const override {
+        return false;
+    }
+
+    void read_bytes(void* dst, size_t size, size_t offset) const override {
+        throw std::logic_error("tried to read from an EmptyImageDataSource");
+    }
+
+protected:
+
+    DUK_NO_DISCARD duk::hash::Hash calculate_hash() const override {
+        duk::hash::Hash hash = 0;
+        duk::hash::hash_combine(hash, m_width);
+        duk::hash::hash_combine(hash, m_height);
+        duk::hash::hash_combine(hash, m_pixelFormat);
+        return hash;
+    }
+
+private:
+    uint32_t m_width;
+    uint32_t m_height;
+    Image::PixelFormat m_pixelFormat;
 };
 
 }
