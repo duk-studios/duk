6f33c51d30bbd395fd4bce54946089b014ea1650
Ricardo Novak <ricardonovak@yahoo.com.br>
2023-06-10 20:34:32 +0100

    new: add more glm basic type hashes

--- duk_hash/include/duk_hash/common_hashes.h
+++ duk_hash/include/duk_hash/common_hashes.h
@@ -46,6 +46,72 @@ struct hash<glm::vec4> {
     }
 };
 
+template<>
+struct hash<glm::u32vec2> {
+    size_t operator()(const glm::u32vec2& vec) noexcept {
+        size_t hash = 0;
+        duk::hash::hash_combine(hash, vec.x);
+        duk::hash::hash_combine(hash, vec.y);
+        return hash;
+    }
+};
+
+template<>
+struct hash<glm::u32vec3> {
+    size_t operator()(const glm::u32vec3& vec) noexcept {
+        size_t hash = 0;
+        duk::hash::hash_combine(hash, vec.x);
+        duk::hash::hash_combine(hash, vec.y);
+        duk::hash::hash_combine(hash, vec.z);
+        return hash;
+    }
+};
+
+template<>
+struct hash<glm::u32vec4> {
+    size_t operator()(const glm::u32vec4& vec) noexcept {
+        size_t hash = 0;
+        duk::hash::hash_combine(hash, vec.x);
+        duk::hash::hash_combine(hash, vec.y);
+        duk::hash::hash_combine(hash, vec.z);
+        duk::hash::hash_combine(hash, vec.w);
+        return hash;
+    }
+};
+
+template<>
+struct hash<glm::ivec2> {
+    size_t operator()(const glm::ivec2& vec) noexcept {
+        size_t hash = 0;
+        duk::hash::hash_combine(hash, vec.x);
+        duk::hash::hash_combine(hash, vec.y);
+        return hash;
+    }
+};
+
+template<>
+struct hash<glm::ivec3> {
+    size_t operator()(const glm::ivec3& vec) noexcept {
+        size_t hash = 0;
+        duk::hash::hash_combine(hash, vec.x);
+        duk::hash::hash_combine(hash, vec.y);
+        duk::hash::hash_combine(hash, vec.z);
+        return hash;
+    }
+};
+
+template<>
+struct hash<glm::ivec4> {
+    size_t operator()(const glm::ivec4& vec) noexcept {
+        size_t hash = 0;
+        duk::hash::hash_combine(hash, vec.x);
+        duk::hash::hash_combine(hash, vec.y);
+        duk::hash::hash_combine(hash, vec.z);
+        duk::hash::hash_combine(hash, vec.w);
+        return hash;
+    }
+};
+
 }
 
 #endif //DUK_HASH_COMMON_HASHES_H
