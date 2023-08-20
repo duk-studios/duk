f56cfa3e9065258e1aed65a11ec8bb01dc1b89fb
Ricardo Novak <ricardonovak@yahoo.com.br>
2023-08-20 09:57:34 +0100

    new: inherit from Renderer in ForwardRenderer

--- duk_renderer/include/duk_renderer/forward/forward_renderer.h
+++ duk_renderer/include/duk_renderer/forward/forward_renderer.h
@@ -5,26 +5,25 @@
 #define DUK_RENDERER_FORWARD_RENDERER_H
 
 #include <duk_scene/scene.h>
+#include <duk_renderer/renderer.h>
 #include <duk_rhi/rhi.h>
 
 namespace duk::renderer {
 
 struct ForwardRendererCreateInfo {
-
+    RendererCreateInfo rendererCreateInfo;
 };
 
-class ForwardRenderer {
+class ForwardRenderer : public Renderer {
 public:
 
     explicit ForwardRenderer(const ForwardRendererCreateInfo& forwardRendererCreateInfo);
 
-    void render(duk::scene::Scene* scene);
+    void render(duk::scene::Scene* scene) override;
 
 private:
-    std::shared_ptr<duk::rhi::RHI> m_rhi;
-    std::shared_ptr<duk::rhi::CommandQueue> m_mainQueue;
-    std::shared_ptr<duk::rhi::CommandScheduler> m_scheduler;
     std::shared_ptr<duk::rhi::RenderPass> m_renderPass;
+    std::shared_ptr<duk::rhi::Image> m_depthImage;
     std::shared_ptr<duk::rhi::FrameBuffer> m_frameBuffer;
 };
 
--- duk_renderer/src/duk_renderer/forward/forward_renderer.cpp
+++ duk_renderer/src/duk_renderer/forward/forward_renderer.cpp
@@ -5,11 +5,68 @@
 
 namespace duk::renderer {
 
-ForwardRenderer::ForwardRenderer(const ForwardRendererCreateInfo& forwardRendererCreateInfo) {
+ForwardRenderer::ForwardRenderer(const ForwardRendererCreateInfo& forwardRendererCreateInfo) :
+    Renderer(forwardRendererCreateInfo.rendererCreateInfo) {
 
+    {
+        m_depthImage = create_depth_image(render_width(), render_height());
+    }
+
+    {
+        duk::rhi::AttachmentDescription colorAttachmentDescription = {};
+        colorAttachmentDescription.format = m_rhi->present_image()->format();
+        colorAttachmentDescription.initialLayout = duk::rhi::Image::Layout::UNDEFINED;
+        colorAttachmentDescription.layout = duk::rhi::Image::Layout::COLOR_ATTACHMENT;
+        colorAttachmentDescription.finalLayout = duk::rhi::Image::Layout::PRESENT_SRC;
+        colorAttachmentDescription.storeOp = duk::rhi::StoreOp::STORE;
+        colorAttachmentDescription.loadOp = duk::rhi::LoadOp::CLEAR;
+
+        duk::rhi::AttachmentDescription depthAttachmentDescription = {};
+        depthAttachmentDescription.format = m_depthImage->format();
+        depthAttachmentDescription.initialLayout = duk::rhi::Image::Layout::UNDEFINED;
+        depthAttachmentDescription.layout = duk::rhi::Image::Layout::DEPTH_STENCIL_ATTACHMENT;
+        depthAttachmentDescription.finalLayout = duk::rhi::Image::Layout::DEPTH_STENCIL_ATTACHMENT;
+        depthAttachmentDescription.storeOp = duk::rhi::StoreOp::DONT_CARE;
+        depthAttachmentDescription.loadOp = duk::rhi::LoadOp::CLEAR;
+
+        duk::rhi::AttachmentDescription attachmentDescriptions[] = {colorAttachmentDescription};
+
+        duk::rhi::RHI::RenderPassCreateInfo renderPassCreateInfo = {};
+        renderPassCreateInfo.colorAttachments = attachmentDescriptions;
+        renderPassCreateInfo.colorAttachmentCount = std::size(attachmentDescriptions);
+        renderPassCreateInfo.depthAttachment = &depthAttachmentDescription;
+
+        auto expectedRenderPass = m_rhi->create_render_pass(renderPassCreateInfo);
+
+        if (!expectedRenderPass) {
+            throw std::runtime_error("failed to create RenderPass: " + expectedRenderPass.error().description());
+        }
+
+        m_renderPass = std::move(expectedRenderPass.value());
+    }
+
+    {
+        duk::rhi::Image* frameBufferAttachments[] = {m_rhi->present_image(), m_depthImage.get()};
+
+        duk::rhi::RHI::FrameBufferCreateInfo frameBufferCreateInfo = {};
+        frameBufferCreateInfo.attachmentCount = std::size(frameBufferAttachments);
+        frameBufferCreateInfo.attachments = frameBufferAttachments;
+        frameBufferCreateInfo.renderPass = m_renderPass.get();
+
+        auto expectedFrameBuffer = m_rhi->create_frame_buffer(frameBufferCreateInfo);
+
+        if (!expectedFrameBuffer) {
+            throw std::runtime_error("failed to create FrameBuffer: " + expectedFrameBuffer.error().description());
+        }
+
+        m_frameBuffer = std::move(expectedFrameBuffer.value());
+    }
 }
 
 void ForwardRenderer::render(duk::scene::Scene* scene) {
+
+    m_rhi->prepare_frame();
+
     auto mainRenderPass = m_mainQueue->submit([this, scene](duk::rhi::CommandBuffer* commandBuffer) {
         commandBuffer->begin();
 
@@ -26,8 +83,6 @@ void ForwardRenderer::render(duk::scene::Scene* scene) {
 
         commandBuffer->end();
     });
-
-    m_rhi->prepare_frame();
 
     m_scheduler->begin();
 
