0ad93e8444b62501d7c3a85947e7b2cd2fdfc463
Ricardo Novak <ricardonovak@yahoo.com.br>
2023-09-23 14:34:02 +0100

    new: add camera UBO class

---
+++ duk_renderer/include/duk_renderer/painters/camera.h
@@ -0,0 +1,24 @@
+/// 23/09/2023
+/// camera.h
+
+#ifndef DUK_RENDERER_CAMERA_H
+#define DUK_RENDERER_CAMERA_H
+
+#include <duk_renderer/painters/uniform_buffer.h>
+
+#include <glm/matrix.hpp>
+
+namespace duk::renderer {
+
+struct CameraMatrices {
+    glm::mat4 view;
+    glm::mat4 proj;
+    glm::mat4 vp;
+};
+
+using CameraUBO = UniformBuffer<CameraMatrices>;
+
+}
+
+#endif // DUK_RENDERER_CAMERA_H
+
---
+++ duk_renderer/src/duk_renderer/painters/camera.cpp
@@ -0,0 +1,5 @@
+/// 23/09/2023
+/// camera.cpp
+
+#include <duk_renderer/painters/camera.h>
+
