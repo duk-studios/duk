e0d2d318d64236b9cf1e4579c132395fa74a1de6
Ricardo Novak <ricardonovak@yahoo.com.br>
2023-10-12 10:41:41 +0100

    chg: precalculate descriptor hash on construction

--- duk_rhi/src/duk_rhi/descriptor.cpp
+++ duk_rhi/src/duk_rhi/descriptor.cpp
@@ -14,6 +14,7 @@ Descriptor Descriptor::uniform_buffer(Buffer* buffer) {
     Descriptor descriptor;
     descriptor.m_data.bufferDescriptor.buffer = buffer;
     descriptor.m_type = DescriptorType::UNIFORM_BUFFER;
+    descriptor.update_hash();
     return descriptor;
 }
 
@@ -22,6 +23,7 @@ Descriptor Descriptor::storage_buffer(Buffer* buffer) {
     Descriptor descriptor;
     descriptor.m_data.bufferDescriptor.buffer = buffer;
     descriptor.m_type = DescriptorType::STORAGE_BUFFER;
+    descriptor.update_hash();
     return descriptor;
 }
 
@@ -30,6 +32,7 @@ Descriptor Descriptor::image(Image* image, Image::Layout layout) {
     descriptor.m_data.imageDescriptor.image = image;
     descriptor.m_data.imageDescriptor.layout = layout;
     descriptor.m_type = DescriptorType::IMAGE;
+    descriptor.update_hash();
     return descriptor;
 }
 
@@ -39,6 +42,7 @@ Descriptor Descriptor::image_sampler(Image* image, Image::Layout layout, Sampler
     descriptor.m_data.imageDescriptor.layout = layout;
     descriptor.m_data.imageDescriptor.sampler = sampler;
     descriptor.m_type = DescriptorType::IMAGE_SAMPLER;
+    descriptor.update_hash();
     return descriptor;
 }
 
@@ -47,25 +51,13 @@ Descriptor Descriptor::storage_image(Image* image, Image::Layout layout) {
     descriptor.m_data.imageDescriptor.image = image;
     descriptor.m_data.imageDescriptor.layout = layout;
     descriptor.m_type = DescriptorType::STORAGE_IMAGE;
+    descriptor.update_hash();
     return descriptor;
 }
 
 Descriptor::Descriptor() {
     m_type = DescriptorType::UNDEFINED;
-}
-
-Resource::HashChangedEvent& Descriptor::hash_changed_event() const {
-    switch (m_type) {
-        case DescriptorType::UNIFORM_BUFFER:
-        case DescriptorType::STORAGE_BUFFER:
-            return m_data.bufferDescriptor.buffer->hash_changed_event();
-        case DescriptorType::IMAGE:
-        case DescriptorType::IMAGE_SAMPLER:
-        case DescriptorType::STORAGE_IMAGE:
-            return m_data.imageDescriptor.image->hash_changed_event();
-        default:
-            throw std::logic_error("tried to get HashChangedEvent from an undefined descriptor");
-    }
+    m_hash = 0;
 }
 
 DescriptorType Descriptor::type() const {
@@ -73,25 +65,7 @@ DescriptorType Descriptor::type() const {
 }
 
 duk::hash::Hash Descriptor::hash() const {
-    duk::hash::Hash hash = 0;
-    switch (m_type) {
-        case DescriptorType::UNIFORM_BUFFER:
-        case DescriptorType::STORAGE_BUFFER:
-            duk::hash::hash_combine(hash, m_data.bufferDescriptor.buffer);
-            duk::hash::hash_combine(hash, m_data.bufferDescriptor.buffer->hash());
-            break;
-        case DescriptorType::IMAGE_SAMPLER:
-            duk::hash::hash_combine(hash, m_data.imageDescriptor.sampler);
-        case DescriptorType::IMAGE:
-        case DescriptorType::STORAGE_IMAGE:
-            duk::hash::hash_combine(hash, m_data.imageDescriptor.layout);
-            duk::hash::hash_combine(hash, m_data.imageDescriptor.image);
-            duk::hash::hash_combine(hash, m_data.imageDescriptor.image->hash());
-            break;
-        default:
-            break;
-    }
-    return hash;
+    return m_hash;
 }
 
 Image* Descriptor::image() const {
@@ -112,6 +86,30 @@ Sampler Descriptor::sampler() const {
 Buffer* Descriptor::buffer() const {
     assert(m_type == DescriptorType::UNIFORM_BUFFER || m_type == DescriptorType::STORAGE_BUFFER);
     return m_data.bufferDescriptor.buffer;
+}
+
+bool Descriptor::operator==(const Descriptor& rhs) const noexcept {
+    return m_hash == rhs.m_hash;
+}
+
+void Descriptor::update_hash() {
+    duk::hash::Hash hash = 0;
+    switch (m_type) {
+        case DescriptorType::UNIFORM_BUFFER:
+        case DescriptorType::STORAGE_BUFFER:
+            duk::hash::hash_combine(hash, m_data.bufferDescriptor.buffer);
+            break;
+        case DescriptorType::IMAGE_SAMPLER:
+            duk::hash::hash_combine(hash, m_data.imageDescriptor.sampler);
+        case DescriptorType::IMAGE:
+        case DescriptorType::STORAGE_IMAGE:
+            duk::hash::hash_combine(hash, m_data.imageDescriptor.layout);
+            duk::hash::hash_combine(hash, m_data.imageDescriptor.image);
+            break;
+        default:
+            break;
+    }
+    m_hash = hash;
 }
 
 }
