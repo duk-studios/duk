fdb60d7006b135ff662920600f970ecf82293802
Ricardo Novak <ricardonovak@yahoo.com.br>
2023-10-14 23:02:09 +0100

    chg: reorganize code in painter_generator main.cpp

--- duk_renderer/tools/duk_painter_generator/src/duk_painter_generator/main.cpp
+++ duk_renderer/tools/duk_painter_generator/src/duk_painter_generator/main.cpp
@@ -1,6 +1,43 @@
 #include <duk_painter_generator/parser.h>
 #include <duk_painter_generator/reflector.h>
+#include <duk_painter_generator/generator.h>
+
 #include <iostream>
+
+static void print_reflector(duk::painter_generator::Reflector& reflector) {
+    const auto& typeMap = reflector.type_map();
+
+    std::cout << "--------------------------" << std::endl;
+    std::cout << "-----------types----------" << std::endl;
+    std::cout << "--------------------------" << std::endl;
+
+    for (const auto& [typeName, type] : typeMap) {
+
+        std::cout << "-" << typeName << std::endl;
+        std::cout << "- size:        " << type.size << std::endl;
+        std::cout << "- memberCount: " << type.members.size() << std::endl;
+        for (const auto& member : type.members) {
+            std::cout << "-- type: " << member.typeName << " name: " << member.name << std::endl;
+            std::cout << "-- size: " << member.size << " paddedSize: " << member.paddedSize << " offset: " << member.offset << std::endl;
+            if (member.arraySize) {
+                std::cout << "-- arraySize: " << member.arraySize << std::endl;
+            }
+        }
+    }
+
+    std::cout << "--------------------------" << std::endl;
+    std::cout << "---------bindings---------" << std::endl;
+    std::cout << "--------------------------" << std::endl;
+
+    const auto& sets = reflector.sets();
+    for (const auto& set : sets) {
+        std::cout << "- set binding count: " << set.bindings.size() << std::endl;
+        for (const auto& [bindingIndex, binding] : set.bindings) {
+            std::cout << "-- type: " << binding.typeName << " name: " << binding.name << std::endl;
+            std::cout << "-- binding: " << binding.binding << " set: " << binding.set << std::endl;
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
 
@@ -9,38 +46,9 @@ int main(int argc, char* argv[]) {
 
         duk::painter_generator::Reflector reflector(parser);
 
-        const auto& typeMap = reflector.type_map();
+        print_reflector(reflector);
 
-        std::cout << "--------------------------" << std::endl;
-        std::cout << "-----------types----------" << std::endl;
-        std::cout << "--------------------------" << std::endl;
-
-        for (const auto& [typeName, type] : typeMap) {
-
-            std::cout << "-" << typeName << std::endl;
-            std::cout << "- size:        " << type.size << std::endl;
-            std::cout << "- memberCount: " << type.members.size() << std::endl;
-            for (const auto& member : type.members) {
-                std::cout << "-- type: " << member.typeName << " name: " << member.name << std::endl;
-                std::cout << "-- size: " << member.size << " paddedSize: " << member.paddedSize << " offset: " << member.offset << std::endl;
-                if (member.arraySize) {
-                    std::cout << "-- arraySize: " << member.arraySize << std::endl;
-                }
-            }
-        }
-
-        std::cout << "--------------------------" << std::endl;
-        std::cout << "---------bindings---------" << std::endl;
-        std::cout << "--------------------------" << std::endl;
-
-        const auto& sets = reflector.sets();
-        for (const auto& set : sets) {
-            std::cout << "- set binding count: " << set.bindings.size() << std::endl;
-            for (const auto& [bindingIndex, binding] : set.bindings) {
-                std::cout << "-- type: " << binding.typeName << " name: " << binding.name << std::endl;
-                std::cout << "-- binding: " << binding.binding << " set: " << binding.set << std::endl;
-            }
-        }
+        duk::painter_generator::Generator generator(parser, reflector);
     }
     catch (std::exception& e) {
         std::cerr << "Error: " << e.what();
