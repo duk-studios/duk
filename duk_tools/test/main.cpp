34435de9ab3843ee3df433472aeeaaf495870111
Ricardo Novak <ricardonovak@yahoo.com.br>
2023-11-17 11:42:03 +0000

    chg: update duk_tools test

--- duk_tools/test/main.cpp
+++ duk_tools/test/main.cpp
@@ -4,13 +4,8 @@
 
 #include <duk_tools/bit_block.h>
 #include <iostream>
-#include <bitset>
-#include <bit>
 
 int main() {
-
-    std::bitset<128> bitset;
-
     duk::tools::BitBlock<128> bitBlock;
     for (int i = 0; i < 64; i++) {
         bitBlock.set(i);
@@ -20,6 +15,11 @@ int main() {
 
     std::cout << "BitBlock: " << bitBlock.to_string() << std::endl;
     std::cout << "Set bit indices: ";
+    for (auto bit : bitBlock.bits<true>()) {
+        std::cout << bit << ',';
+    }
+    std::cout << std::endl;
+    std::cout << "Unset bit indices: ";
     duk::tools::for_each_bit<false>(bitBlock, [](size_t bitIndex) {
         std::cout << bitIndex << ',';
     });
