5f178235ed3519c2d08348a8dd6556e07523be97
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-01-02 22:34:22 +0000

    new: #29: add basic from_json types

--- duk_import/include/duk_import/json/types.h
+++ duk_import/include/duk_import/json/types.h
@@ -33,6 +33,51 @@ duk::rhi::Sampler to_sampler(const rapidjson::Value& member);
 
 duk::renderer::LightValue to_light_value(const rapidjson::Value& member);
 
+template<typename T>
+void from_json(const rapidjson::Value& jsonObject, T& object) {
+    object = jsonObject.Get<T>();
+}
+
+template<>
+void from_json<glm::vec2>(const rapidjson::Value& jsonObject, glm::vec2& object) {
+    object = to_vec2(jsonObject);
+}
+
+template<>
+void from_json<glm::vec3>(const rapidjson::Value& jsonObject, glm::vec3& object) {
+    object = to_vec3(jsonObject);
+}
+
+template<>
+void from_json<glm::vec4>(const rapidjson::Value& jsonObject, glm::vec4& object) {
+    object = to_vec4(jsonObject);
+}
+
+template<>
+void from_json<duk::pool::ResourceId>(const rapidjson::Value& jsonObject, duk::pool::ResourceId& object) {
+    object = to_resource_id(jsonObject);
+}
+
+template<>
+void from_json<duk::rhi::Sampler::WrapMode>(const rapidjson::Value& jsonObject, duk::rhi::Sampler::WrapMode& object) {
+    object = to_wrap_mode(jsonObject);
+}
+
+template<>
+void from_json<duk::rhi::Sampler::Filter>(const rapidjson::Value& jsonObject, duk::rhi::Sampler::Filter& object) {
+    object = to_filter(jsonObject);
+}
+
+template<>
+void from_json<duk::rhi::Sampler>(const rapidjson::Value& jsonObject, duk::rhi::Sampler& object) {
+    object = to_sampler(jsonObject);
+}
+
+template<>
+void from_json<duk::renderer::LightValue>(const rapidjson::Value& jsonObject, duk::renderer::LightValue& object) {
+    object = to_light_value(jsonObject);
+}
+
 }
 
 #endif //DUK_IMPORT_TYPES_H
