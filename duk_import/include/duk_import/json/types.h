2d44a06b50ed44c44cc61f7b1e11885d2f7d9a4c
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-01-03 17:05:29 +0000

    fix: #29: fix compilation issues with from_json methods

--- duk_import/include/duk_import/json/types.h
+++ duk_import/include/duk_import/json/types.h
@@ -39,42 +39,42 @@ void from_json(const rapidjson::Value& jsonObject, T& object) {
 }
 
 template<>
-void from_json<glm::vec2>(const rapidjson::Value& jsonObject, glm::vec2& object) {
+inline void from_json<glm::vec2>(const rapidjson::Value& jsonObject, glm::vec2& object) {
     object = to_vec2(jsonObject);
 }
 
 template<>
-void from_json<glm::vec3>(const rapidjson::Value& jsonObject, glm::vec3& object) {
+inline void from_json<glm::vec3>(const rapidjson::Value& jsonObject, glm::vec3& object) {
     object = to_vec3(jsonObject);
 }
 
 template<>
-void from_json<glm::vec4>(const rapidjson::Value& jsonObject, glm::vec4& object) {
+inline void from_json<glm::vec4>(const rapidjson::Value& jsonObject, glm::vec4& object) {
     object = to_vec4(jsonObject);
 }
 
 template<>
-void from_json<duk::pool::ResourceId>(const rapidjson::Value& jsonObject, duk::pool::ResourceId& object) {
+inline void from_json<duk::pool::ResourceId>(const rapidjson::Value& jsonObject, duk::pool::ResourceId& object) {
     object = to_resource_id(jsonObject);
 }
 
 template<>
-void from_json<duk::rhi::Sampler::WrapMode>(const rapidjson::Value& jsonObject, duk::rhi::Sampler::WrapMode& object) {
+inline void from_json<duk::rhi::Sampler::WrapMode>(const rapidjson::Value& jsonObject, duk::rhi::Sampler::WrapMode& object) {
     object = to_wrap_mode(jsonObject);
 }
 
 template<>
-void from_json<duk::rhi::Sampler::Filter>(const rapidjson::Value& jsonObject, duk::rhi::Sampler::Filter& object) {
+inline void from_json<duk::rhi::Sampler::Filter>(const rapidjson::Value& jsonObject, duk::rhi::Sampler::Filter& object) {
     object = to_filter(jsonObject);
 }
 
 template<>
-void from_json<duk::rhi::Sampler>(const rapidjson::Value& jsonObject, duk::rhi::Sampler& object) {
+inline void from_json<duk::rhi::Sampler>(const rapidjson::Value& jsonObject, duk::rhi::Sampler& object) {
     object = to_sampler(jsonObject);
 }
 
 template<>
-void from_json<duk::renderer::LightValue>(const rapidjson::Value& jsonObject, duk::renderer::LightValue& object) {
+inline void from_json<duk::renderer::LightValue>(const rapidjson::Value& jsonObject, duk::renderer::LightValue& object) {
     object = to_light_value(jsonObject);
 }
 
