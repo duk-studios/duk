88a7741b5c00e1f0b821239cf1b2f6e80228d305
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-01-11 20:57:06 +0000

    chg: #44: use volk in duk_rhi

--- duk_rhi/src/duk_rhi/vulkan/vulkan_import.h
+++ duk_rhi/src/duk_rhi/vulkan/vulkan_import.h
@@ -5,16 +5,6 @@
 #ifndef DUK_VULKAN_IMPORT_H
 #define DUK_VULKAN_IMPORT_H
 
-#if DUK_PLATFORM_IS_WINDOWS
-#define VK_USE_PLATFORM_WIN32_KHR
-#elif DUK_PLATFORM_IS_LINUX
-#define VK_USE_PLATFORM_XLIB_KHR
-#else
-#error Unsupported platform for vulkan
-#endif
-
-#include <vulkan/vulkan.h>
-
-
+#include <volk.h>
 
 #endif //DUK_VULKAN_IMPORT_H
