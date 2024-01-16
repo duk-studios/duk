af10ffdee89195f123c8c01e02e16764068e6975
sidao <sidneicarrarojunio@gmail.com>
2024-01-16 14:54:33 -0300

    new: #13: update-duk_log-readme

--- duk_log/test/main.cpp
+++ duk_log/test/main.cpp
@@ -1,10 +1,4 @@
-# duk_log
-- Logging module, exposes a simple interface that auto initializes when first used.
-- Has the concept of Loggers and Sinks, which can be expanded to any kind of output.
-
-# Usage
-```cpp
-#include <duk_log/logger.h>
+﻿#include <duk_log/logger.h>
 #include <duk_log/log.h>
 
 #include "duk_log/sink_cout.h"
@@ -25,6 +19,3 @@ int main() {
     
     return 0;
 }
-
-    
-```
