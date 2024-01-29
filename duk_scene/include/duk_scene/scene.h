d26703b707a844add180c5d73007b7098084c46b
JuniorSidnei <sidneicarrarojunior@gmail.com>
2024-01-29 16:58:30 -0300

    new: #20: update duk scene readme (#63)
    Co-authored-by: sidao <sidneicarrarojunio@gmail.com>

--- duk_scene/include/duk_scene/scene.h
+++ duk_scene/include/duk_scene/scene.h
@@ -214,7 +214,7 @@ void Scene::add_component(const Object::Id& id, Args&& ... args) {
 
 template<typename T>
 void Scene::remove_component(const Object::Id& id) {
-    assert(contains<T>(id));
+    assert(valid_component<T>(id));
     remove_component(id.index(), component_index<T>());
     m_objectComponentMasks[id.index()].reset(component_index<T>());
 }
