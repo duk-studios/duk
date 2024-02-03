88fbf81687028a1e044340baabb4725c25d44afe
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-02-03 17:37:07 +0000

    new: #68: add duk_json module

---
+++ duk_json/include/duk_json/string.h
@@ -0,0 +1,16 @@
+/// 03/02/2024
+/// string.h
+
+#ifndef DUK_JSON_STRING_H
+#define DUK_JSON_STRING_H
+
+#include <rapidjson/document.h>
+
+namespace duk::json {
+
+std::string to_string(const rapidjson::Value& value);
+
+}
+
+#endif // DUK_JSON_STRING_H
+
