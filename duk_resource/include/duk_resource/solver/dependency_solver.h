36a1c2170b0026c68ca26f4ae5ac18371863b000
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-02-09 17:28:50 +0000

    new: #68: add DependencySolver

--- duk_resource/include/duk_resource/solver/dependency_solver.h
+++ duk_resource/include/duk_resource/solver/dependency_solver.h
@@ -4,5 +4,41 @@
 #ifndef DUK_RESOURCE_DEPENDENCY_SOLVER_H
 #define DUK_RESOURCE_DEPENDENCY_SOLVER_H
 
-#endif // DUK_RESOURCE_DEPENDENCY_SOLVER_H
+#include <duk_resource/resource.h>
+#include <duk_resource/solver/solver.h>
+#include <set>
 
+namespace duk::resource {
+
+class DependencySolver {
+public:
+
+    template<typename T>
+    void solve(ResourceT<T>& resource);
+
+    template<typename T>
+    void solve(T& object);
+
+    void clear();
+
+    size_t size() const;
+
+    DUK_NO_DISCARD const std::set<Id>& dependencies() const;
+
+private:
+    std::set<Id> m_dependencies;
+};
+
+template<typename T>
+void DependencySolver::solve(ResourceT<T>& resource) {
+    m_dependencies.insert(resource.id());
+}
+
+template<typename T>
+void DependencySolver::solve(T& object) {
+    solve_resources(this, object);
+}
+
+}
+
+#endif // DUK_RESOURCE_DEPENDENCY_SOLVER_H
--- duk_resource/src/duk_resource/solver/dependency_solver.cpp
+++ duk_resource/src/duk_resource/solver/dependency_solver.cpp
@@ -3,3 +3,18 @@
 
 #include <duk_resource/solver/dependency_solver.h>
 
+namespace duk::resource {
+
+void DependencySolver::clear() {
+    m_dependencies.clear();
+}
+
+size_t DependencySolver::size() const {
+    return m_dependencies.size();
+}
+
+const std::set<Id>& DependencySolver::dependencies() const {
+    return m_dependencies;
+}
+
+}
