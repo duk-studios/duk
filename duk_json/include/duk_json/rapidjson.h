ec2261c9a4ab63e00cb85d533600bd091c494212
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-02-24 13:35:49 +0000

    chg: #101: use duk_assert for rapidjson assertion

---
+++ duk_json/include/duk_json/rapidjson.h
@@ -0,0 +1,12 @@
+/// 24/02/2024
+/// rapidjson.h
+
+#ifndef DUK_JSON_RAPIDJSON_H
+#define DUK_JSON_RAPIDJSON_H
+
+#include <duk_macros/assert.h>
+
+#define RAPIDJSON_ASSERT(x) DUK_ASSERT(x)
+#include <rapidjson/document.h>
+
+#endif// DUK_JSON_RAPIDJSON_H
