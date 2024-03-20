03933807a91b2dd9faab2c1a9e49fe20f49d9970
Ricardo Cicaida <ricardonovak@yahoo.com.br>
2024-03-20 21:07:30 +0000

    new: DUK-31: implement cmake install for relevant modules (#126)

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -2,3 +2,7 @@ if (CMAKE_GENERATOR MATCHES "Ninja")
     set(VCPKG_TARGET_TRIPLET x64-mingw-static)
 endif()
 include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
+
+if (NOT CMAKE_DEBUG_POSTFIX)
+    set(CMAKE_DEBUG_POSTFIX d)
+endif ()
