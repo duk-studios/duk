5a9709ab905310ad14debc2fbba6936f7cb25dbc
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-03-22 20:58:13 +0000

    chg: DUK-43: only install vcpkg if requested

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -1,8 +1,5 @@
 if (CMAKE_GENERATOR MATCHES "Ninja")
     set(VCPKG_TARGET_TRIPLET x64-mingw-static)
 endif()
+set(VCPKG_MANIFEST_DIR ${DUK_ROOT})
 include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
-
-if (NOT CMAKE_DEBUG_POSTFIX)
-    set(CMAKE_DEBUG_POSTFIX d)
-endif ()
