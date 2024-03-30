d035b2b95cddd65eb0e1c726ff599c34227cbc17
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-03-30 17:18:49 +0000

    new: DUK-52: add runtime resource support

--- duk_resource/include/duk_resource/resource.h
+++ duk_resource/include/duk_resource/resource.h
@@ -71,6 +71,8 @@ public:
 
     ResourceT(Id id, const std::shared_ptr<T>& resource);
 
+    ResourceT(const std::shared_ptr<T>& resource);
+
     template<typename U>
     ResourceT(const ResourceT<U>& other)
         requires std::is_base_of_v<T, U>;
@@ -118,6 +120,13 @@ template<typename T>
 ResourceT<T>::ResourceT(Id id, const std::shared_ptr<T>& resource)
     : Resource(id)
     , m_resource(resource) {
+}
+
+template<typename T>
+ResourceT<T>::ResourceT(const std::shared_ptr<T>& resource)
+    : Resource(kInvalidId)
+    , m_resource(resource) {
+
 }
 
 template<typename T>
