e770884500b73a3042dadc59ed5d4ffbd71547b0
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-04-14 18:42:27 +0100

    new: DUK-57: use vcpkg deps install

--- cmake/vcpkg.cmake
+++ cmake/vcpkg.cmake
@@ -1,7 +1,9 @@
 include_guard()
 
 if (CMAKE_GENERATOR MATCHES "Ninja")
-    set(VCPKG_TARGET_TRIPLET x64-mingw-static)
+    set(VCPKG_TARGET_TRIPLET x64-mingw-dynamic)
 endif()
+
+set(X_VCPKG_APPLOCAL_DEPS_INSTALL TRUE CACHE BOOL "Copy dependencies to install target directory")
 set(VCPKG_MANIFEST_DIR ${DUK_ROOT})
 include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
