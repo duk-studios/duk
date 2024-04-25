a0b898ea050c5b29a1f82dcac754779abe4507bd
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-04-25 14:02:56 +0100

    new: DUK-53: update sprite shader to use instancing

--- duk_renderer/src/duk_renderer/material/sprites/sprite_color/sprite_color_material.cpp
+++ duk_renderer/src/duk_renderer/material/sprites/sprite_color/sprite_color_material.cpp
@@ -5,6 +5,7 @@
 #include <duk_renderer/material/globals/global_descriptors.h>
 #include <duk_renderer/material/sprites/sprite_color/sprite_color_material.h>
 #include <duk_renderer/renderer.h>
+#include <duk_renderer/components/transform.h>
 
 namespace duk::renderer {
 
@@ -37,6 +38,35 @@ hash::Hash SpriteColorMaterialDataSource::calculate_hash() const {
     return hash;
 }
 
+SpriteColorInstanceBuffer::SpriteColorInstanceBuffer(const SpriteColorInstanceBufferCreateInfo& spriteColorInstanceBufferCreateInfo) {
+    auto rhi = spriteColorInstanceBufferCreateInfo.renderer->rhi();
+    auto commandQueue = spriteColorInstanceBufferCreateInfo.renderer->main_command_queue();
+
+    StorageBufferCreateInfo transformSBOCreateInfo = {};
+    transformSBOCreateInfo.rhi = rhi;
+    transformSBOCreateInfo.commandQueue = commandQueue;
+    transformSBOCreateInfo.initialSize = 1;
+    m_instancesSBO = std::make_unique<StorageBuffer<SpriteColorDescriptors::Instance>>(transformSBOCreateInfo);
+}
+
+void SpriteColorInstanceBuffer::insert(const duk::objects::Object& object) {
+    auto transform = object.component<Transform>();
+    auto& instance = m_instancesSBO->next();
+    instance.model = transform->model;
+}
+
+void SpriteColorInstanceBuffer::clear() {
+    m_instancesSBO->clear();
+}
+
+void SpriteColorInstanceBuffer::flush() {
+    m_instancesSBO->flush();
+}
+
+const StorageBuffer<SpriteColorDescriptors::Instance>* SpriteColorInstanceBuffer::instances() const {
+    return m_instancesSBO.get();
+}
+
 SpriteColorMaterialDescriptorSet::SpriteColorMaterialDescriptorSet(const SpriteColorMaterialDescriptorSetCreateInfo& spriteColorMaterialCreateInfo) {
     auto renderer = spriteColorMaterialCreateInfo.renderer;
     auto rhi = renderer->rhi();
@@ -49,13 +79,23 @@ SpriteColorMaterialDescriptorSet::SpriteColorMaterialDescriptorSet(const SpriteC
         m_descriptorSet = rhi->create_descriptor_set(descriptorSetCreateInfo);
     }
     {
+        SpriteColorInstanceBufferCreateInfo spriteColorInstanceBufferCreateInfo = {};
+        spriteColorInstanceBufferCreateInfo.renderer = renderer;
+        m_instanceBuffer = std::make_unique<SpriteColorInstanceBuffer>(spriteColorInstanceBufferCreateInfo);
+        m_descriptorSet->set(SpriteColorDescriptors::uInstances, m_instanceBuffer->instances()->descriptor());
+    }
+    {
         UniformBufferCreateInfo<SpriteColorDescriptors::Material> materialUboCreateInfo = {};
         materialUboCreateInfo.rhi = rhi;
         materialUboCreateInfo.commandQueue = commandQueue;
         materialUboCreateInfo.initialData = {spriteColorMaterialCreateInfo.spriteColorMaterialDataSource->color};
 
         m_materialUBO = std::make_unique<UniformBuffer<SpriteColorDescriptors::Material>>(materialUboCreateInfo);
+        m_descriptorSet->set(SpriteColorDescriptors::uMaterial, m_materialUBO->descriptor());
     }
+
+    Texture texture{materialDataSource->image, {duk::rhi::Sampler::Filter::NEAREST, duk::rhi::Sampler::WrapMode::CLAMP_TO_EDGE}};
+    m_descriptorSet->set(SpriteColorDescriptors::uBaseColor, texture.descriptor());
 }
 
 void SpriteColorMaterialDescriptorSet::set_color(const glm::vec4& color) {
@@ -79,8 +119,13 @@ bool SpriteColorMaterialDescriptorSet::is_image(uint32_t index) {
     return index == SpriteColorDescriptors::uBaseColor;
 }
 
+InstanceBuffer* SpriteColorMaterialDescriptorSet::instance_buffer() {
+    return m_instanceBuffer.get();
+}
+
 void SpriteColorMaterialDescriptorSet::update(const DrawParams& params) {
     m_descriptorSet->set(SpriteColorDescriptors::uCamera, params.globalDescriptors->camera_ubo()->descriptor());
+    m_descriptorSet->set(SpriteColorDescriptors::uInstances, m_instanceBuffer->instances()->descriptor());
     m_descriptorSet->flush();
 }
 
