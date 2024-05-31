acc1e1c8eaa149135b546f39a8b35e7350a77b06
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-05-31 17:24:52 +0100

    new: DUK-84: improve duk.cmake to use environment variables properly

--- cmake/duk.cmake
+++ cmake/duk.cmake
@@ -1,26 +1,38 @@
-set(CURRENT_DIR "${DUK_ROOT}/cmake")
+# this file should be included in your project:
+# include(your/path/to/duk.cmake)
 
-include("${CURRENT_DIR}/vcpkg.cmake")
 if (DUK_AS_SOURCE)
     message(STATUS "including source duk")
-    option(DUK_BUILD_SAMPLE "Disables building of duk_sample" OFF)
-    add_subdirectory("${DUK_ROOT}" "${CMAKE_CURRENT_BINARY_DIR}/duk")
-else ()
-    message(STATUS "including prebuilt duk")
-    set(DUK_TOOLCHAIN)
-    if (CMAKE_CXX_COMPILER MATCHES "g\\+\\+" OR CMAKE_CXX_COMPILER MATCHES "clang")
-        set(DUK_TOOLCHAIN "mingw")
-    elseif (MSVC)
-        set(DUK_TOOLCHAIN "vs2022")
-    else ()
-        message(FATAL_ERROR "Failed find a valid duk toolchain for compiler")
+    set(DUK_SOURCE_DIR $ENV{DUK_SOURCE_ROOT})
+
+    if (NOT DUK_SOURCE_DIR)
+        message(FATAL_ERROR "DUK_SOURCE_ROOT is not defined")
     endif ()
 
-    set(DUK_CONFIG_FILE "${DUK_ROOT}/install/${DUK_TOOLCHAIN}/share/duk/duk-config.cmake")
+    if (NOT EXISTS "${DUK_SOURCE_DIR}")
+        message(FATAL_ERROR "DUK_SOURCE_ROOT \"${DUK_SOURCE_DIR}\" does not exist")
+    endif ()
+
+    option(DUK_BUILD_SAMPLE "Disables building of duk_sample" OFF)
+    add_subdirectory("${DUK_SOURCE_DIR}" "${CMAKE_CURRENT_BINARY_DIR}/duk")
+    include("${DUK_SOURCE_DIR}/cmake/compile_shaders.cmake")
+else ()
+    message(STATUS "including installed duk")
+    set(DUK_INSTALL_DIR $ENV{DUK_INSTALL_ROOT})
+
+    if (NOT DUK_INSTALL_DIR)
+        message(FATAL_ERROR "DUK_INSTALL_ROOT is not defined")
+    endif ()
+
+    if (NOT EXISTS "${DUK_INSTALL_DIR}")
+        message(FATAL_ERROR "DUK_INSTALL_ROOT \"${DUK_INSTALL_DIR}\" does not exist")
+    endif ()
+
+    set(DUK_CONFIG_FILE "${DUK_INSTALL_DIR}/share/duk/duk-config.cmake")
 
     if (EXISTS "${DUK_CONFIG_FILE}")
         include("${DUK_CONFIG_FILE}")
     else ()
-        message(FATAL_ERROR "Could not find duk-config.cmake at \"${DUK_CONFIG_FILE}\", please install duk for toolchain \"${DUK_TOOLCHAIN}\"")
+        message(FATAL_ERROR "Could not find duk-config.cmake at \"${DUK_CONFIG_FILE}\", please check your duk installation")
     endif ()
 endif ()
