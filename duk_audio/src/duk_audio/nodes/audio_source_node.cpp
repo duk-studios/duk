3372f25557229413622fa91445b9a30667a174b7
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-06-18 20:08:12 +0100

    fix: DUK-116: resample audio outside of audio buffer

--- duk_audio/src/duk_audio/nodes/audio_source_node.cpp
+++ duk_audio/src/duk_audio/nodes/audio_source_node.cpp
@@ -13,30 +13,75 @@ namespace duk::audio {
 namespace detail {
 
 static void mix_float_samples(float* dst, const float* src, uint32_t frameCount, uint32_t channelCount) {
-    for (uint32_t i = 0; i < frameCount; i++) {
-        uint32_t frameIndex = i * channelCount;
-        for (uint32_t c = 0; c < channelCount; c++) {
-            float sample = dst[frameIndex + c] + src[frameIndex + c];
-            dst[frameIndex + c] = std::tanh(sample);
-        }
+    for (uint32_t i = 0; i < frameCount * channelCount; i++) {
+        dst[i] = std::tanh(dst[i] + src[i]);
     }
 }
 
-static void process_buffer_float(const AudioBuffer* buffer, float* output, uint32_t frameCount, uint32_t channelCount, uint32_t& currentFrame, float frameRate, bool loop) {
+static uint32_t read_buffer_float(const AudioBuffer* buffer, float* output, uint32_t& currentFrame, uint32_t frameCount, uint32_t channelCount, bool loop) {
     uint32_t framesRead = 0;
     float* dst = output;
-    while (framesRead < frameCount && (currentFrame < buffer->frame_count() || loop)) {
-        auto readAmount = buffer->read_float(dst, frameCount - framesRead, currentFrame, channelCount, frameRate);
+    while (framesRead < frameCount) {
+        auto readAmount = buffer->read_float(dst, frameCount - framesRead, currentFrame, channelCount);
         dst += readAmount * channelCount;
         framesRead += readAmount;
         currentFrame += readAmount;
 
-        if (loop && currentFrame >= buffer->frame_count()) {
+        if (currentFrame >= buffer->frame_count()) {
+            if (!loop) {
+                break;
+            }
             currentFrame = 0;
         }
     }
-    if (framesRead < frameCount) {
-        memset(output + framesRead * channelCount, 0, (frameCount - framesRead) * channelCount * sizeof(float));
+    return framesRead;
+}
+
+// todo: remove this function once we have duk_math
+template<typename T>
+static T lerp(T a, T b, float t) {
+    return a + (b - a) * t;
+}
+
+static void resample_buffer_float(const AudioBuffer* buffer, float* output, uint32_t& currentFrame, uint32_t frameCount, uint32_t channelCount, float frameRate, bool loop) {
+    const uint32_t kReadBufferSampleCount = 1024;
+    const uint32_t kReadBufferFrameCount = kReadBufferSampleCount / channelCount;
+    float readBuffer[kReadBufferSampleCount];
+    auto writeBuffer = output;
+    uint32_t remainingFrames = frameCount;
+    while (remainingFrames) {
+        auto remainingRawFramesToRead = static_cast<uint32_t>(static_cast<float>(remainingFrames) * frameRate);
+        auto rawFramesToRead = std::min(remainingRawFramesToRead, kReadBufferFrameCount);
+        auto rawFramesRead = read_buffer_float(buffer, readBuffer, currentFrame, rawFramesToRead, channelCount, loop);
+        if (!rawFramesRead) {
+            break;
+        }
+
+        auto resampledFramesRead = static_cast<uint32_t>(static_cast<float>(rawFramesRead) * (1.0f / frameRate));
+
+        if (!resampledFramesRead) {
+            break;
+        }
+
+        // resample to output buffer
+        for (auto channel = 0; channel < channelCount; channel++) {
+            for (uint32_t i = 0; i < resampledFramesRead; i++) {
+                auto writeSample = i * channelCount + channel;
+                auto readFrameIndex = static_cast<float>(i) * frameRate;
+                auto readFrame0 = static_cast<uint32_t>(std::floor(readFrameIndex));
+                auto readFrameFrac = readFrameIndex - static_cast<float>(readFrame0);
+                auto readSample0 = readFrame0 * channelCount + channel;
+                if (readFrame0 + 1 < rawFramesRead) {
+                    auto readFrame1 = readFrame0 + 1;
+                    auto readSample1 = readFrame1 * channelCount + channel;
+                    writeBuffer[writeSample] = lerp(readBuffer[readSample0], readBuffer[readSample1], readFrameFrac);
+                } else {
+                    writeBuffer[writeSample] = readBuffer[readSample0];
+                }
+            }
+        }
+        writeBuffer += resampledFramesRead * channelCount;
+        remainingFrames -= resampledFramesRead;
     }
 }
 
@@ -57,10 +102,26 @@ static void process_slot(AudioSourceNode::Slot& slot, const std::shared_ptr<Audi
     float* dstFrames = (float*)output;
     uint32_t remainingFrames = frameCount;
 
+    slot.deviceVolume = slot.hostVolume;
+    slot.deviceFrameRate = slot.hostFrameRate;
+    slot.deviceLoop = slot.hostLoop;
+    if (slot.deviceFrameRate <= 0.0f) {
+        return;
+    }
+    {
+        std::lock_guard lock(slot.syncMutex);
+        if (slot.syncDeviceFrame) {
+            slot.deviceFrame = slot.hostFrame;
+            slot.syncDeviceFrame = false;
+        } else {
+            slot.hostFrame = slot.deviceFrame;
+        }
+    }
+
     while (remainingFrames) {
         auto framesToRead = std::min(remainingFrames, kBufferFrameCount);
 
-        process_buffer_float(buffer.get(), outputBuffer, framesToRead, channelCount, slot.deviceFrame, slot.deviceFrameRate, slot.deviceLoop);
+        resample_buffer_float(buffer.get(), outputBuffer, slot.deviceFrame, framesToRead, channelCount, slot.deviceFrameRate, slot.deviceLoop);
 
         apply_volume(outputBuffer, slot.deviceVolume, framesToRead, channelCount);
 
@@ -69,20 +130,9 @@ static void process_slot(AudioSourceNode::Slot& slot, const std::shared_ptr<Audi
 
         remainingFrames -= framesToRead;
     }
-
-    slot.deviceVolume = slot.hostVolume;
-    slot.deviceFrameRate = slot.hostFrameRate;
-    slot.deviceLoop = slot.hostLoop;
-    std::lock_guard lock(slot.syncMutex);
-    if (slot.syncDeviceFrame) {
-        slot.deviceFrame = slot.hostFrame;
-        slot.syncDeviceFrame = false;
-    } else {
-        slot.hostFrame = slot.deviceFrame;
-    }
 }
 
-static void update_slot(AudioSourceNode::Slot& slot, const std::shared_ptr<AudioBuffer>& buffer, float volume, float frameRate, bool loop, uint32_t priority) {
+static void update_slot(AudioSourceNode::Slot& slot, const std::shared_ptr<AudioBuffer>& buffer, float volume, float frameRate, bool loop, int32_t priority) {
     slot.buffer = buffer;
     slot.priority = priority;
     slot.hostVolume = volume;
