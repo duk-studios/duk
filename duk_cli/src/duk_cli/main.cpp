5ea0df3ba1172c140d86301f9f7c9080177b31fd
Ricardo Cicaida <ricardonovak@yahoo.com.br>
2024-07-29 18:11:55 +0100

    fix: DUK-141: initialize cli logging (#209)
    * new: DUK-141: add default platform initialization
    
    * fix: DUK-141: initialize platform and logging on duk_cli

--- duk_cli/src/duk_cli/main.cpp
+++ duk_cli/src/duk_cli/main.cpp
@@ -5,11 +5,19 @@
 #include <duk_cli/command_line.h>
 
 #include <duk_log/log.h>
+#include <duk_log/cout_sink.h>
 
 #include <duk_project/register_types.h>
+#include <duk_platform/win32/platform_win_32.h>
 
 int main(int argc, const char* argv[]) {
+    // initialize platform specifics (like console virtualization on win32)
+    auto platform = duk::platform::create_default_platform();
+
     try {
+        duk::log::add_logger("duk", duk::log::INFO);
+        duk::log::add_sink(std::make_unique<duk::log::CoutSink>("duk-cout", duk::log::INFO));
+
         duk::project::register_types();
 
         duk::cli::CommandLine commandLine(argc - 1, argv + 1);
