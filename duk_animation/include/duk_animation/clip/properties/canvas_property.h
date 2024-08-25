2228bd2a51e3f8914f30689bb56742bea42002eb
Ricardo Novak <ricardonovak@yahoo.com.br>
2024-08-25 17:15:45 +0100

    chg: DUK-145: remove obsolete canvas animation properties

--- duk_animation/include/duk_animation/clip/properties/canvas_property.h
+++ duk_animation/include/duk_animation/clip/properties/canvas_property.h
@@ -9,19 +9,6 @@
 
 namespace duk::animation {
 
-struct CanvasAnchorValue {
-    glm::vec2 anchor;
-};
-
-class CanvasAnchorProperty {
-public:
-    using ValueType = CanvasAnchorValue;
-
-    static void evaluate(const duk::objects::Object& object, const PropertyT<CanvasAnchorProperty>* property, uint32_t sample);
-
-    static ValueType interpolate(const ValueType& from, const ValueType& to, float progress);
-};
-
 struct CanvasPivotValue {
     glm::vec2 pivot;
 };
@@ -31,32 +18,6 @@ public:
     using ValueType = CanvasPivotValue;
 
     static void evaluate(const duk::objects::Object& object, const PropertyT<CanvasPivotProperty>* property, uint32_t sample);
-
-    static ValueType interpolate(const ValueType& from, const ValueType& to, float progress);
-};
-
-struct CanvasPositionValue {
-    glm::vec2 position;
-};
-
-class CanvasPositionProperty {
-public:
-    using ValueType = CanvasPositionValue;
-
-    static void evaluate(const duk::objects::Object& object, const PropertyT<CanvasPositionProperty>* property, uint32_t sample);
-
-    static ValueType interpolate(const ValueType& from, const ValueType& to, float progress);
-};
-
-struct CanvasSizeValue {
-    glm::vec2 size;
-};
-
-class CanvasSizeProperty {
-public:
-    using ValueType = CanvasSizeValue;
-
-    static void evaluate(const duk::objects::Object& object, const PropertyT<CanvasSizeProperty>* property, uint32_t sample);
 
     static ValueType interpolate(const ValueType& from, const ValueType& to, float progress);
 };
@@ -79,16 +40,6 @@ public:
 namespace duk::serial {
 
 template<>
-inline void from_json(const rapidjson::Value& value, duk::animation::CanvasAnchorValue& canvas) {
-    from_json_member(value, "anchor", canvas.anchor);
-}
-
-template<>
-inline void to_json(rapidjson::Document& document, rapidjson::Value& value, const duk::animation::CanvasAnchorValue& canvas) {
-    to_json_member(document, value, "anchor", canvas.anchor);
-}
-
-template<>
 inline void from_json(const rapidjson::Value& value, duk::animation::CanvasPivotValue& canvas) {
     from_json_member(value, "pivot", canvas.pivot);
 }
@@ -96,26 +47,6 @@ inline void from_json(const rapidjson::Value& value, duk::animation::CanvasPivot
 template<>
 inline void to_json(rapidjson::Document& document, rapidjson::Value& value, const duk::animation::CanvasPivotValue& canvas) {
     to_json_member(document, value, "pivot", canvas.pivot);
-}
-
-template<>
-inline void from_json(const rapidjson::Value& value, duk::animation::CanvasPositionValue& canvas) {
-    from_json_member(value, "position", canvas.position);
-}
-
-template<>
-inline void to_json(rapidjson::Document& document, rapidjson::Value& value, const duk::animation::CanvasPositionValue& canvas) {
-    to_json_member(document, value, "position", canvas.position);
-}
-
-template<>
-inline void from_json(const rapidjson::Value& value, duk::animation::CanvasSizeValue& canvas) {
-    from_json_member(value, "size", canvas.size);
-}
-
-template<>
-inline void to_json(rapidjson::Document& document, rapidjson::Value& value, const duk::animation::CanvasSizeValue& canvas) {
-    to_json_member(document, value, "size", canvas.size);
 }
 
 template<>
