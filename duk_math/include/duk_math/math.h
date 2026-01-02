849107791667475371256ca407927eacf6e31eaf
Ricardo Novak <ricardonovak@yahoo.com.br>
2026-01-02 17:55:24 +0100

    chg: include duk_math/glm.h in duk_math/math.h

--- duk_math/include/duk_math/math.h
+++ duk_math/include/duk_math/math.h
@@ -5,16 +5,7 @@
 #ifndef DUK_MATH_MATH_H
 #define DUK_MATH_MATH_H
 
-#include <glm/gtc/random.hpp>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/ext/matrix_clip_space.hpp>
-#include <glm/gtc/quaternion.hpp>
-#include <glm/mat4x4.hpp>
-#include <glm/vec2.hpp>
-#include <glm/vec3.hpp>
-#include <glm/vec4.hpp>
-#include <glm/ext/scalar_constants.hpp>
+#include <duk_math/glm.h>
 
 namespace duk::math {
 
