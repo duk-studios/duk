f20639f9ff16c532a8573f8c72e1943b3c65c26b
Ricardo Novak <ricardonovak@yahoo.com.br>
2026-01-10 17:20:19 +0100

    chg: refactor resource solving to use duk_type

--- duk_resource/include/duk_resource/solver/solver.h
+++ duk_resource/include/duk_resource/solver/solver.h
@@ -4,25 +4,91 @@
 #ifndef DUK_RESOURCE_SOLVER_H
 #define DUK_RESOURCE_SOLVER_H
 
+#include <duk_type/describe_class.h>
+#include <duk_type/describe_container.h>
+
 namespace duk::resource {
 
 /*
- * Implement this method for types that have any resources references (or has members that reference resources):
-
-namespace duk::resource {
-
-template<typename Solver>
-void solve_resources(Solver* solver, YourType& obj) {
-    solver->solve(obj.resource1);
-    solver->solve(obj.resource2);
-    solver->solve(obj.typeThatHasResourceMembers);
-}
-
-}
+ * Types that specialize duk::type::Type<T> can be solved for resources using this function.
+ * It will recursively visit all members and elements of the object and call the solver's solve method on them.
+ * Some primitives (such as smart pointers) have specializations to dereference and solve the underlying object.
  */
 
 template<typename Solver, typename T>
+void solve_resources(Solver* solver, T& obj);
+
+template<typename Solver, typename T>
+struct ObjectResourceSolver {
+    static void solve(Solver* solver, T& obj);
+};
+
+template<typename Solver, typename T>
+struct ContainerResourceSolver {
+    static void solve(Solver* solver, T& container);
+};
+
+template<typename Solver, typename T>
+struct PrimitiveResourceSolver {
+    static void solve(Solver* solver, T& obj);
+};
+
+template<typename Solver, typename T>
+struct PrimitiveResourceSolver<Solver, std::shared_ptr<T>> {
+    static void solve(Solver* solver, std::shared_ptr<T>& obj);
+};
+
+template<typename Solver, typename T>
+struct PrimitiveResourceSolver<Solver, std::unique_ptr<T>> {
+    static void solve(Solver* solver, std::unique_ptr<T>& obj);
+};
+
+template<typename Solver, typename T>
 void solve_resources(Solver* solver, T& obj) {
+    if constexpr (duk::type::is_class<T>()) {
+        ObjectResourceSolver<Solver, T>::solve(solver, obj);
+    }
+    else if constexpr (duk::type::is_container<T>()) {
+        ContainerResourceSolver<Solver, T>::solve(solver, obj);
+    }
+    else {
+        PrimitiveResourceSolver<Solver, T>::solve(solver, obj);
+    }
+}
+
+template<typename Solver, typename T>
+void ObjectResourceSolver<Solver, T>::solve(Solver* solver, T& obj) {
+    constexpr auto description = duk::type::describe<T>();
+    description.visit_members([&](const auto& member) {
+        solver->solve(member.value());
+    }, obj);
+}
+
+template<typename Solver, typename T>
+void ContainerResourceSolver<Solver, T>::solve(Solver* solver, T& container) {
+    constexpr auto description = duk::type::describe<T>();
+    description.visit_elements([&](auto& element) {
+        solver->solve(element);
+    }, container);
+}
+
+template<typename Solver, typename T>
+void PrimitiveResourceSolver<Solver, T>::solve(Solver* solver, T& obj) {
+    // no-op for primitive types
+}
+
+template<typename Solver, typename T>
+void PrimitiveResourceSolver<Solver, std::shared_ptr<T>>::solve(Solver* solver, std::shared_ptr<T>& obj) {
+    if (obj) {
+        solve_resources(solver, *obj);
+    }
+}
+
+template<typename Solver, typename T>
+void PrimitiveResourceSolver<Solver, std::unique_ptr<T>>::solve(Solver* solver, std::unique_ptr<T>& obj) {
+    if (obj) {
+        solve_resources(solver, *obj);
+    }
 }
 
 }// namespace duk::resource
--- duk_resource/test/solver.cc
+++ duk_resource/test/solver.cc
@@ -15,17 +15,13 @@ struct Bar {
     duk::resource::Handle<Foo> fooHandle;
 };
 
-namespace duk::resource {
-
-template<typename Solver>
-void solve_resources(Solver* solver, Bar& bar) {
-    solver->solve(bar.fooHandle);
-}
-
-}
+struct Baz {
+    std::vector<duk::resource::Handle<Bar>> barHandles;
+};
 
 namespace duk::type {
 
+// clang-format off
 template<>
 struct Type<Foo> : Class<Foo,
     Member<"fooValue", &Foo::fooValue>> {
@@ -36,41 +32,80 @@ struct Type<Bar> : Class<Bar,
     Member<"fooHandle", &Bar::fooHandle>> {
 };
 
+template<>
+struct Type<Baz> : Class<Baz,
+    Member<"barHandles", &Baz::barHandles>> {
+};
+// clang-format on
+
 }
 
 TEST_CASE("We can query dependencies and fix up resource references", "[resource]") {
     using namespace duk::resource;
-    SECTION("Dependency solver") {
-        DependencySolver solver;
-        SECTION("Resource handles are tracked as dependencies") {
-            Handle<Foo> fooHandle(Id(1));
-            solver.solve(fooHandle);
-            CHECK(solver.dependencies().size() == 1);
-            CHECK(solver.dependencies().count(Id(1)) == 1);
-        }
-        SECTION("Types can be solved for resource handles") {
-            Bar bar;
-            bar.fooHandle = Handle<Foo>(Id(2));
-            solver.solve(bar);
-            CHECK(solver.dependencies().size() == 1);
-            CHECK(solver.dependencies().count(Id(2)) == 1);
-        }
+    DependencySolver dependencySolver;
+    Pools pools;
+    ReferenceSolver referenceSolver(pools);
+    SECTION("Resource handles are tracked as dependencies") {
+        Handle<Foo> fooHandle(Id(1));
+        dependencySolver.solve(fooHandle);
+        CHECK(dependencySolver.dependencies().size() == 1);
+        CHECK(dependencySolver.dependencies().count(Id(1)) == 1);
     }
-    SECTION("Reference solver") {
-        Pools pools;
-        ReferenceSolver solver(pools);
-        SECTION("Resource handles can be fixed up") {
-            auto fooResource = std::make_shared<Foo>();
-            fooResource->fooValue = 99;
-            auto fooHandle = pools.insert<Foo>(Id(3), fooResource);
-
-            Bar bar;
-            bar.fooHandle = Handle<Foo>(Id(3)); // initially just an id
-
-            solver.solve(bar);
-
-            REQUIRE(bar.fooHandle.valid());
-            CHECK(bar.fooHandle->fooValue == 99);
-        }
+    SECTION("Types can be solved for resource handles") {
+        Bar bar;
+        bar.fooHandle = Handle<Foo>(Id(2));
+        dependencySolver.solve(bar);
+        CHECK(dependencySolver.dependencies().size() == 1);
+        CHECK(dependencySolver.dependencies().count(Id(2)) == 1);
     }
+    SECTION("Resource handles can be fixed up") {
+        auto fooResource = std::make_shared<Foo>();
+        fooResource->fooValue = 99;
+        auto fooHandle = pools.insert<Foo>(Id(3), fooResource);
+
+        Bar bar;
+        bar.fooHandle = Handle<Foo>(Id(3)); // initially just an id
+
+        referenceSolver.solve(bar);
+
+        REQUIRE(bar.fooHandle.valid());
+        CHECK(bar.fooHandle->fooValue == 99);
+    }
+    SECTION("Containers of resource handles can be fixed up") {
+        auto fooResource1 = std::make_shared<Foo>();
+        fooResource1->fooValue = 11;
+        auto fooHandle1 = pools.insert<Foo>(Id(4), fooResource1);
+
+        auto fooResource2 = std::make_shared<Foo>();
+        fooResource2->fooValue = 22;
+        auto fooHandle2 = pools.insert<Foo>(Id(5), fooResource2);
+
+        // Create Bars that reference the Foos
+        auto barResource1 = std::make_shared<Bar>();
+        barResource1->fooHandle = Handle<Foo>(Id(4));
+        pools.insert<Bar>(Id(6), barResource1);
+
+        auto barResource2 = std::make_shared<Bar>();
+        barResource2->fooHandle = Handle<Foo>(Id(5));
+        pools.insert<Bar>(Id(7), barResource2);
+
+        // Now solve the Bars to fix up their Foo references
+        referenceSolver.solve(barResource1);
+        referenceSolver.solve(barResource2);
+
+        Baz baz;
+        baz.barHandles.push_back(Handle<Bar>(Id(6)));
+        baz.barHandles.push_back(Handle<Bar>(Id(7)));
+
+        referenceSolver.solve(baz);
+
+        REQUIRE(baz.barHandles.size() == 2);
+        REQUIRE(baz.barHandles[0].valid());
+        REQUIRE(baz.barHandles[1].valid());
+        REQUIRE(baz.barHandles[0]->fooHandle.valid());
+        REQUIRE(baz.barHandles[1]->fooHandle.valid());
+        CHECK(baz.barHandles[0]->fooHandle->fooValue == 11);
+        CHECK(baz.barHandles[1]->fooHandle->fooValue == 22);
+    }
+
 }
