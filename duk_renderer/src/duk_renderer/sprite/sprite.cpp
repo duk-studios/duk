2f82467147385c838fcf1420f6195e9f7ecef3ca
Ricardo Novak <ricardonovak@yahoo.com.br>
2026-04-26 11:47:02 +0200

    fix: compute default metrics if provided invalid sprite index

--- duk_renderer/src/duk_renderer/sprite/sprite.cpp
+++ duk_renderer/src/duk_renderer/sprite/sprite.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <duk_renderer/sprite/sprite.h>
+#include <duk_macros/assert.h>
 
 namespace duk::renderer {
 
@@ -10,6 +11,8 @@ Sprite::Sprite(const SpriteAtlasCreateInfo& spriteAtlasCreateInfo)
     : m_image(spriteAtlasCreateInfo.spriteAtlasData->image)
     , m_pixelsPerUnit(spriteAtlasCreateInfo.spriteAtlasData->pixelsPerUnit)
     , m_sprites(spriteAtlasCreateInfo.spriteAtlasData->sprites) {
+    DUK_ASSERT(!m_sprites.empty() && "Sprite atlas must contain at least one sprite");
+    DUK_ASSERT(m_pixelsPerUnit > 0.0f && "Pixels per unit must be greater than zero");
 }
 
 ImageResource& Sprite::image() {
@@ -25,20 +28,26 @@ uint32_t Sprite::count() const {
 }
 
 SpriteMetrics Sprite::compute_metrics(uint32_t spriteIndex) const {
-    DUK_ASSERT(spriteIndex < m_sprites.size());
-    const auto imageSize = glm::vec2(m_image->size());
-    const auto& sprite = m_sprites[spriteIndex];
-
-    const auto spriteMin = glm::vec2(sprite.min.x, sprite.min.y);
-    const auto spriteMax = glm::vec2(sprite.max.x, sprite.max.y);
-    const auto spriteSize = (spriteMax - spriteMin) / m_pixelsPerUnit;
-    const auto minOffset = spriteSize * sprite.pivot;
-
     SpriteMetrics metrics = {};
-    metrics.uv.min = spriteMin / imageSize;
-    metrics.uv.max = spriteMax / imageSize;
-    metrics.position.min = -minOffset;
-    metrics.position.max = spriteSize - minOffset;
+    if (spriteIndex < m_sprites.size() && m_image.valid()) {
+        const auto imageSize = glm::vec2(m_image->size());
+        const auto& sprite = m_sprites[spriteIndex];
+
+        const auto spriteMin = glm::vec2(sprite.min.x, sprite.min.y);
+        const auto spriteMax = glm::vec2(sprite.max.x, sprite.max.y);
+        const auto spriteSize = (spriteMax - spriteMin) / m_pixelsPerUnit;
+        const auto minOffset = spriteSize * sprite.pivot;
+
+        metrics.uv.min = spriteMin / imageSize;
+        metrics.uv.max = spriteMax / imageSize;
+        metrics.position.min = -minOffset;
+        metrics.position.max = spriteSize - minOffset;
+    } else {
+        metrics.uv.min = {0.0f, 0.0f};
+        metrics.uv.max = {1.0f, 1.0f};
+        metrics.position.min = {-0.5f, -0.5f};
+        metrics.position.max = {0.5f, 0.5f};
+    }
 
     return metrics;
 }
